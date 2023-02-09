b1499b8e0a10da7a2fcc334e3a378d38a871f7b7
Aida <aidachernyavskaya@gmail.com>
2023-02-09 18:02:59 +0300

    App layout done

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Button, Checkbox, Input, Ptag, DateContainer, Task, Add, Calendar} from 
 import getDate from "date-fns/getDate";
 import getISODay from 'date-fns/getISODay';
 import getMonth from 'date-fns/getMonth';
+import {inspect} from "util";
 
 
 function App() {
@@ -11,40 +12,15 @@ function App() {
     const weekdays = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
     const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
     return (
-        <div className="App">
-            <Ptag size={"b"} color={"black"}>Hello big black</Ptag>
-            <Ptag size={"m"} color={"gray"}>Hello medium gray</Ptag>
-            <Ptag size={"s"} color={"blue"}>Hello small blue</Ptag>
+        <div className="app__container">
+            <Add className='add'/>
+            <Calendar className="calendar"/>
+            {/*<DateContainer day={getDate(date)} weekday={weekdays[getISODay(date)]} month={months[getMonth(date)]} empty={true} active={true}/>*/}
 
-            <Input
-                background={true}
-                placeholder={"Введите задачу"}
-            />
-            <Input
-                background={false}
-                placeholder={"HERE IS task written"}
-            />
-            <Input
-                background={false}
-                placeholder={"task"}
-                checked={true}
-            />
+            <Task className="task" task={'read'}/>
+            <Task className="task" task={'do smth'}/>
+            <Task className="task" task={'eat'}/>
 
-            <Button icon={"plus"} appearance={'gray'}/>
-            <Button icon={"del"} appearance={'ghost'}/>
-            <Button icon={"arrowLeft"} appearance={'ghost'}/>
-            <Button icon={"arrowRight"} appearance={'ghost'}/>
-            <Button icon={"drag"} appearance={'ghost'}/>
-            <Button icon={"edit"} appearance={'ghost'}/>
-            <Button icon={"close"} appearance={'ghost'}/>
-
-            <Checkbox/>
-            <DateContainer day={getDate(date)} weekday={weekdays[getISODay(date)]} month={months[getMonth(date)]} empty={true} active={true}/>
-            <DateContainer day={15} weekday={'чт'} month={"Декабрь"} empty={false} active={false}/>
-
-            <Task task={'read'}/>
-            <Add/>
-            <Calendar/>
         </div>
   );
 }
