22fe1934f5cd1b75bd1d2c74b5376c183b762839
Aida <aidachernyavskaya@gmail.com>
2023-03-01 20:08:56 +0300

    calendar fill according to width

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -3,31 +3,51 @@ import styles from "./Calendar.module.css";
 import cn from "classnames";
 import {Button} from "../Button/Button";
 import {DateContainer} from "../DateContainer/DateContainer";
-import React from "react";
+import React, {MutableRefObject, useEffect, useRef, useState} from "react";
 import getDate from "date-fns/getDate";
 import getISODay from 'date-fns/getISODay';
 import getMonth from 'date-fns/getMonth';
 import {currentDate} from "../../App";
 
-export const createDays = (startDate: Date): Array<Date> => {
+export const createDays = (startDate: Date, daysCount: number): Array<Date> => {
     const daysArray: Array<Date> = [];
-    for (let i = 0; i < 10; i++){
+    for (let i = 0; i < daysCount; i++){
         daysArray[i] = new Date();
         daysArray[i].setDate(startDate.getDate() + i);
     }
     return daysArray;
 };
 
+export const calculateDaysCount = (widthContainer: number): number => {
+    const WIDTH_DATE_ELEM = 80;
+    return Math.floor(widthContainer / WIDTH_DATE_ELEM);
+};
+
 export const Calendar = ({className, ...props}: CalendarProps): JSX.Element => {
-    const days: Array<Date> = createDays(currentDate);
-    console.log(currentDate, 'currentDate');
     const weekdays = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
     const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+
+    const [days, setDays] = useState<Array<Date>>([]);
+
+    useEffect(() => {
+        const datesContainerElem = document.getElementById("datesContainer");
+        if (datesContainerElem) {
+            new ResizeObserver(resizeListener).observe(datesContainerElem);
+        }
+    }, []);
+
+    const resizeListener = (): void => {
+        const datesContainerElem = document.getElementById("datesContainer");
+        if (datesContainerElem){
+            const daysCount = calculateDaysCount(datesContainerElem.offsetWidth);
+            setDays(createDays(currentDate, daysCount));
+        }
+    };
 
     return(
         <div className={cn(className, styles.calendar__container)} {...props}>
             <Button icon={'arrowLeft'} appearance={'ghost'}/>
-            <div className={styles.calendar}>
+            <div className={styles.calendar} id={"datesContainer"} >
                 {days.map((day, index) => (
                     <DateContainer
                         day={getDate(day)}
